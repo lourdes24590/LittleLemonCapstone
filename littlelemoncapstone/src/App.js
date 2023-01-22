ab64c00a65fcddd411f83bd0c72aa2e3aec4ac10
Lourdes Torrejon <lourdestorrejon@Lourdess-MacBook-Pro.local>
2023-01-22 12:00:47 -0500

    Progress on app

--- littlelemoncapstone/src/App.js
+++ littlelemoncapstone/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import React from 'react';
+import Footer from './Footer.js';
+import Main from './Main.js';
+import Nav from './Nav.js';
+import Header from './Header.js';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <React.Fragment>
+      <body>
+      <Header/>
+      <Nav/>
+      <Main/>
+      <Footer/>
+      </body>
+    </React.Fragment>
   );
 }
 
 export default App;
+
---
+++ littlelemoncapstone/src/Footer.js
@@ -0,0 +1,23 @@
+import './styles.css';
+function Footer() {
+    return (
+      <footer>
+        <div><img src="home icon.svg"/></div>
+        <div>
+          <ul>
+          <ul><li>Navigation</li></ul>
+            <li><a href="home.html">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="menu.html">Menu</a></li>
+            <li><a href="reservations.html">Reservations</a></li>
+            <li><a href="orderonline.html">Order Online</a></li>
+            <li><a href="login.html">Login</a></li>
+            </ul>
+        </div>
+        <div><ul><li>Contact</li></ul></div>
+        <div><ul><li>Social Media Links</li></ul></div>
+      </footer>
+    )
+};
+
+export default Footer;
